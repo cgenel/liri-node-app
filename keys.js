bc5b4938c7b1451a69b722f388395a9172fb9841
cgenel <genel661@gmail.com>
2020-01-13 16:49:01 -0800

    spotify id added

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
-
+// spotify client id & secret
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.e96d5abb8b8f4a56b75fc78cde7b0fde,
+  secret: process.env.SPOTIFY_2fc72cf5d2144758983e5448cdb04184
 };
