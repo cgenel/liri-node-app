ad3c1dcc03dbf94e43562ed1d317c72d320042e1
cgenel <genel661@gmail.com>
2020-01-27 14:06:44 -0800

    npm packages installed

--- liri.js
+++ liri.js
@@ -1,1 +1,19 @@
 require("dotenv").config();
+
+// imports the api keys
+var keys = require('./keys.js');
+
+// 
+var Spotify = require("node-spotify-api");
+
+// Import the axios npm package.
+var axios = require("axios");
+
+// Import the moment npm package.
+var moment = require("moment");
+
+// Import the FS package for read/write.
+var fs = require("fs");
+
+// Initialize the spotify API client using our client id and secret
+var spotify = new Spotify(keys.spotify);
