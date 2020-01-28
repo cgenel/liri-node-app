3d1ca8ccea27ef5c7ccfeb53d16d98f5cf7d9a18
cgenel <genel661@gmail.com>
2020-01-27 16:16:32 -0800

    spotify search function created

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-console.log('this is loaded');
+console.log("this is loaded");
 // spotify client id & secret
 exports.spotify = {
   id: process.env.e96d5abb8b8f4a56b75fc78cde7b0fde,
-  secret: process.env.SPOTIFY_2fc72cf5d2144758983e5448cdb04184
+  secret: process.env.aab40a12bb104d4c864c77469101acce
 };
--- liri.js
+++ liri.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 // imports the api keys
-var keys = require('./keys.js');
+var keys = require("./keys.js");
 
 // 
 var Spotify = require("node-spotify-api");
@@ -17,3 +17,44 @@ var fs = require("fs");
 
 // Initialize the spotify API client using our client id and secret
 var spotify = new Spotify(keys.spotify);
+
+// function to get the artists name
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+
+// function for running searches on Spotify
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
+
+  // when searching spotify for the song name "track"
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+
+    // if error return an error
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+
+      // songs variable for collecting the data
+      var songs = data.tracks.items;
+
+      // for-loop to display all songs with the title entered
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
