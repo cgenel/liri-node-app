aca3ad5c89dbc7a1c1ae80ec57fd911077abcdff
cgenel <genel661@gmail.com>
2020-01-27 16:27:15 -0800

    refresh code, spotify not working

--- keys.js
+++
@@ -1,6 +0,0 @@
-console.log("this is loaded");
-// spotify client id & secret
-exports.spotify = {
-  id: process.env.e96d5abb8b8f4a56b75fc78cde7b0fde,
-  secret: process.env.aab40a12bb104d4c864c77469101acce
-};
